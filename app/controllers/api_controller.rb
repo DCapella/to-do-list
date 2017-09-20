b72e30d18075f112937f68ca505abdae1f8b4388
David Capella <D_Capella@yahoo.com>
2017-09-20 18:52:15 -0500

    fixed layout and clean up codes and comment out before_action :authenticate_user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,17 +3,13 @@ class ApiController < ApplicationController
 
   private
   def authenticated?
-    # authenticate_or_request_with_http_basic {|email, password| User.where( email: email, password: password).present? }
     authenticate_or_request_with_http_basic do |username,password|
+      # Have to make sure user exists first before validating password because
+      # of devise gem.
       if User.where( email: username ).present?
         user = User.find_by_email(username)
         user.valid_password?(password)
       end
-      # resource = User.find_by_email(username)
-      # # resource.valid_password?(password)
-      # if resource.valid_password?(password)
-      #   sign_in :user, resource
-      # end
     end
   end
 end
